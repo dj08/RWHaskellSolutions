01ea481715e06d4f827ff6e1d4870b46ea5e11a6
dj08 <juneja.dushyant@gmail.com>
2016-04-28 18:39:43 +0530

    Practising more IO...

---
+++ RWH-07/callingpure.hs
@@ -0,0 +1,16 @@
+-- file: ch07/callingpure.hs
+name2reply :: String -> String
+name2reply name =
+    "Pleased to meet you, " ++ name ++ ".\n" ++
+    "Your name contains " ++ charcount ++ " characters."
+    where charcount = show (length name)
+
+main :: IO ()
+main = do
+       putStrLn "Greetings once again.  What is your name?"
+       inpStr <- getLine
+       let outStr = name2reply inpStr
+       putStrLn outStr
+
+-- let and <- convert IO t to t
+-- that is, use them to get values for pure computations
